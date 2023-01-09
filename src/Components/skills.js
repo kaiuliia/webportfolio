4eb71f45fa7d56fa9dfcd07a59b59a4b1a923d98
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2023-01-09 09:35:55 +0100

    added all icons

--- src/Components/skills.js
+++ src/Components/skills.js
@@ -1,7 +1,12 @@
 import React from "react";
 import "./skills.css";
-import lkdn from '../resources/icons/fb.png';
-
+import html from "../resources/icons/html.png";
+import css from "../resources/icons/css.png";
+import vs from "../resources/icons/vs.png";
+import git from "../resources/icons/git.png";
+import node from "../resources/icons/node.png";
+import react from "../resources/icons/react.png";
+import js from "../resources/icons/js.png";
 
 export class Skills extends React.Component {
   constructor(props) {
@@ -9,16 +14,39 @@ export class Skills extends React.Component {
   }
 
   render() {
-
     return (
       <div id="skills">
-      
         <div class="skillname">
           <p>HTML</p>
-          <img class="icon" src={lkdn} />
+          <img class="icon" src={html} />
+        </div>
+
+        <div class="skillname">
+          <p>CSS</p>
+          <img class="icon" src={css} />
+           </div>
+
+            <div class="skillname">
+          <p>Java Script</p>
+          <img class="icon" src={js} />
+           </div>
+ <div class="skillname">
+          <p>Node.js</p>
+          <img class="icon" src={node} />
+           </div>
+ <div class="skillname">
+          <p>React</p>
+          <img class="icon" src={react} />
+           </div>
+ <div class="skillname">
+          <p>GitHub</p>
+          <img class="icon" src={git} />
+ </div>
+         <div class="skillname">
+          <p>Visual Studio</p>
+          <img class="icon" src={vs} />
           
-          <p>HTML</p>
-          <img class="icon" src={lkdn} />
+
         </div>
       </div>
     );
