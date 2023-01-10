52d3d4ddbb17151592998e9fc0094471fc526c78
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2023-01-10 17:04:53 +0100

    changed scale component

--- src/Components/scale.js
+++ src/Components/scale.js
@@ -1,18 +1,15 @@
 import React from "react";
 import "./scale.css";
 
-export class Scale extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+export function Scale (props) {
 
-  render() {
-    const fill = this.props.percent;
+ 
+    const fill = props.percent;
     return (
       <div class="wholescale">
         <div class="fillscale" style={{ width: fill }}>
           <div class="namescale">
-            <p>{this.props.name}</p>
+            <p>{props.name}</p>
           </div>
         </div>
         <div class="percent">
@@ -21,4 +18,4 @@ export class Scale extends React.Component {
       </div>
     );
   }
-}
+
