e538042b69cf51b855d4319936368c2b7d8b20b7
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2023-01-10 17:01:35 +0100

    changed links component

--- src/Components/links.js
+++ src/Components/links.js
@@ -1,34 +1,30 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./links.css";
 
-export function Links (props) {
- 
-const [text, setText] = useState(props.name)
+export function Links(props) {
+  const [text, setText] = useState(props.name);
 
   const changeText = () => {
-    setText(props.alt)
+    setText(props.alt);
+  };
 
-  }
-
- const changeBack = () => {
-    setText(props.name)
-  }
+  const changeBack = () => {
+    setText(props.name);
+  };
 
   const openLink = () => {
     window.location.replace(props.link);
-  }
+  };
 
-  render() {
-    const linkName = this.state.text;
-    return (
-      <a
-        id="old"
-        onMouseEnter={changeText}
-        onMouseLeave={changeBack}
-        onClick={openLink}
-      >
-        <h1 id="link">{text} </h1>
-      </a>
-    );
-  }
+  const linkName = text;
+  return (
+    <a
+      id="old"
+      onMouseEnter={changeText}
+      onMouseLeave={changeBack}
+      onClick={openLink}
+    >
+      <h1 id="link">{text} </h1>
+    </a>
+  );
 }
