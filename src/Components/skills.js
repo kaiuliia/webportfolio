9ddaff3668079abad6ac4ebbfa9e3a26da5386e0
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2023-01-10 17:07:04 +0100

    Update skills.js

--- src/Components/skills.js
+++ src/Components/skills.js
@@ -1,12 +1,6 @@
 import React from "react";
 import "./skills.css";
-import html from "../resources/icons/html.png";
-import css from "../resources/icons/css.png";
-import vs from "../resources/icons/vs.png";
-import git from "../resources/icons/git.png";
-import node from "../resources/icons/node.png";
-import react from "../resources/icons/react.png";
-import js from "../resources/icons/js.png";
+
 
 export function Skills(props) {
   return (
