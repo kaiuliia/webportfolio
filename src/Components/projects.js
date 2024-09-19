4108853d1173cfbda7e922066ec43440bdede8bb
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2024-09-19 19:05:39 +0200

    Rewrite first section.

--- src/Components/projects.js
+++ src/Components/projects.js
@@ -29,19 +29,21 @@ export function Projects(props) {
       <div class=" w-full bg-green-500  h-[12rem] rounded-b-none rounded-lg">
         <div></div>
       </div>
-      <div className={"text-sm pt-3  px-2 "}>
-        <h1 className="text-black" onClick={openProject}>
+      <div className={"text-sm py-3 pl-5 "}>
+        <h1 className="text-black font-medium" onClick={openProject}>
           {props.name}
         </h1>
         <br />
         <p>{props.desc}</p>
+        <br />
+        <button
+          onClick={openProject}
+          className="bg-transparent  hover:bg-cyan-700 text-cyan-700 font-semibold hover:text-white py-2 px-4 border border-cyan-700 hover:border-transparent rounded"
+        >
+          Open
+        </button>
       </div>
-      <button
-        onClick={openProject}
-        className="bg-transparent  hover:bg-cyan-700 text-cyan-700 font-semibold hover:text-white py-2 px-4 border border-cyan-700 hover:border-transparent rounded"
-      >
-        Open
-      </button>
+
       <br />
     </div>
   );
